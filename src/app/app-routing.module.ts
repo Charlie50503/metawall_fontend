3d2af8e646082679edc7e5cbfa8ec9714128332a
charlie <cate50503@gmail.com>
2022-09-08 13:51:10 +0900

    feat: 更改main router入口

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,12 @@ import { HeaderComponent } from './components/header/header.component';
 import { LoginComponent } from './pages/login/login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { MainComponent } from './pages/main/main.component';
 
 const routes: Routes = [
   { path: "login", component: LoginComponent},
   { path: "header", component: HeaderComponent},
-  { path: "sidebar", component: SidebarComponent},
+  { path: "main", component: MainComponent},
 ];
 
 @NgModule({
