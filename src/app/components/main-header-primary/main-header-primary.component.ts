b11ca04af34e89d86fd9478f233b115e9fe205d9
charlie <cate50503@gmail.com>
2022-09-13 15:41:22 +0900

    refactor: 重構 追蹤名單，張貼動態的header區塊
    統一成一個component

--- src/app/components/main-header-primary/main-header-primary.component.ts
+++ src/app/components/main-header-primary/main-header-primary.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-main-header-primary',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-header-primary.component.scss']
 })
 export class MainHeaderPrimaryComponent implements OnInit {
-
+  @Input() title: string = "";
   constructor() { }
 
   ngOnInit(): void {
