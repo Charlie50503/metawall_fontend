8e56ee78bfda1b90972f3c2d9ddec3ebe8bf5be8
charlie <cate50503@gmail.com>
2022-09-14 12:22:39 +0900

    feat:添加personal-profile基本內容
    並添加tab控制

--- src/app/components/personal-profile/personal-profile.component.ts
+++ src/app/components/personal-profile/personal-profile.component.ts
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PersonalProfileComponent implements OnInit {
 
+  isDisplayTab:{ [key: string]: boolean } = {
+    personalProfileEdit:true,
+    personalProfileChangePassword:false
+  }
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  toggleIsDisplayTab(targetKey:string){
+    this.isDisplayTab['isPersonalProfileChangePassword'] = true
+    this.isDisplayTab['isPersonalProfileEdit'] = false
+    for (const [key, value] of Object.entries(this.isDisplayTab)) {
+      if(key===targetKey){
+        this.isDisplayTab[key]=true
+      }else{
+        this.isDisplayTab[key]=false
+      }
+    }
+  }
 }
