a9ee7237d608980e5c42af0a381b6920ca1ecc3a
charlie <cate50503@gmail.com>
2022-09-14 11:06:15 +0900

    feat:新增like-user component

---
+++ src/app/components/like-user/like-user.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-like-user',
+  templateUrl: './like-user.component.html',
+  styleUrls: ['./like-user.component.scss']
+})
+export class LikeUserComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
