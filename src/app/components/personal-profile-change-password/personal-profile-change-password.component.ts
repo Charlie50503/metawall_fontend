6bc0a0656518a1608bcbf3f978c5582c59f6d1e7
charlie <cate50503@gmail.com>
2022-09-14 11:38:52 +0900

    feat:新增personal-profile-change-password component

---
+++ src/app/components/personal-profile-change-password/personal-profile-change-password.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-personal-profile-change-password',
+  templateUrl: './personal-profile-change-password.component.html',
+  styleUrls: ['./personal-profile-change-password.component.scss']
+})
+export class PersonalProfileChangePasswordComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
