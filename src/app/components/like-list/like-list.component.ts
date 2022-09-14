a44688fd38b0cf384a4ded38f43837f0f5e9e102
charlie <cate50503@gmail.com>
2022-09-14 11:24:00 +0900

    feat:添加like-list內容

--- src/app/components/like-list/like-list.component.ts
+++ src/app/components/like-list/like-list.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./like-list.component.scss']
 })
 export class LikeListComponent implements OnInit {
-
+  items = [1,2,3]
   constructor() { }
 
   ngOnInit(): void {
