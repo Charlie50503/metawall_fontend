32a0565115ee095359001d2007a2ef4776984245
charlie <cate50503@gmail.com>
2022-09-14 11:39:04 +0900

    feat:新增personal-profile-edit component

---
+++ src/app/components/personal-profile-edit/personal-profile-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-personal-profile-edit',
+  templateUrl: './personal-profile-edit.component.html',
+  styleUrls: ['./personal-profile-edit.component.scss']
+})
+export class PersonalProfileEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
