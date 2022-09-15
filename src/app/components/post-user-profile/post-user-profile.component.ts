3359d3dbb6c60a4e78b38d9715d3144c3aec95bc
charlie <cate50503@gmail.com>
2022-09-15 18:08:29 +0900

    feat: post-user-profile更新
    1. 新增用戶圖片設定，如果用戶沒有設定圖片，會透過性別給予預設的圖片
    2.設定日期格式，因為後端所回傳的日期格式沒有調整過，所以透過前端來調整

--- src/app/components/post-user-profile/post-user-profile.component.ts
+++ src/app/components/post-user-profile/post-user-profile.component.ts
@@ -1,5 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
-
+import { DatetimeFormatService } from './../../services/datetime-format.service';
+import { PostUserProfileService } from './../../services/post-user-profile.service';
+import { Component, Input, OnInit, SimpleChanges } from '@angular/core';
+import { post } from 'src/app/interfaces/post.interface';
 @Component({
   selector: 'app-post-user-profile',
   templateUrl: './post-user-profile.component.html',
@@ -7,9 +9,22 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class PostUserProfileComponent implements OnInit {
   @Input() postUserProfilePhotoClass: string = "";
-  constructor() { }
+  @Input() set createdAt(createdAt:post["createdAt"]){
+    this._createdAt = this.datetimeFormatService.datetimeFormat(createdAt,"yyyy/MM/dd H:m") || ""
+  }
+  @Input() set creator(creator:post["creator"]){
+    this._imgUrl = this.postUserProfileService.setUserImgUrl(creator.avatar,creator.sex)
+  }
+
+  public _imgUrl!:string;
+  public _createdAt!:string;
+  public imgUrl:string = "";
+
+  constructor(
+    private postUserProfileService:PostUserProfileService,
+    private datetimeFormatService:DatetimeFormatService
+  ) { }
 
   ngOnInit(): void {
   }
-
 }
