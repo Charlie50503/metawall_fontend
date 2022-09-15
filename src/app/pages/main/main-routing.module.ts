a907e270ebbac112eef0d7be51ea2abf98131e70
charlie <cate50503@gmail.com>
2022-09-15 15:17:40 +0900

    feat: 添加其他子路由到main畫面

--- src/app/pages/main/main-routing.module.ts
+++ src/app/pages/main/main-routing.module.ts
@@ -1,5 +1,9 @@
+import { PersonalProfileComponent } from './../../components/personal-profile/personal-profile.component';
+import { LikeListComponent } from './../../components/like-list/like-list.component';
+import { PostEditComponent } from './../../components/post-edit/post-edit.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FollowingComponent } from 'src/app/components/following/following.component';
 import { PostListComponent } from 'src/app/components/post-list/post-list.component';
 import { MainComponent } from './main.component';
 
@@ -7,6 +11,10 @@ const routes: Routes = [
   { path: "main", component: MainComponent,
   children: [
     { path: 'all-post', component: PostListComponent },
+    { path: 'following', component: FollowingComponent },
+    { path: 'post-edit', component: PostEditComponent },
+    { path: 'like-list', component: LikeListComponent },
+    { path: 'personal-profile', component: PersonalProfileComponent },
   ]
 },
 ];
