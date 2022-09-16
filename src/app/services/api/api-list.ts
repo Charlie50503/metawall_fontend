09f954d780c5e67f3cfa1b079a356451df0a6adb
charlie50503 <cate50503@gmail.com>
2022-09-16 15:41:35 +0900

    feat: 在api-list添加search post api，並調整all post的參數

--- src/app/services/api/api-list.ts
+++ src/app/services/api/api-list.ts
@@ -1,7 +1,7 @@
 const API_LIST = {
   GET: {
-    ALL_POST: "/post/all-post",
-    SEARCH_POST: (keyword:string) => `/post/?q=${keyword}`,
+    ALL_POST: (sort:string) => `/post/all-post?sort=${sort}`,
+    SEARCH_POST: (keyword:string,sort:string) => `/post/?q=${keyword}&sort=${sort}`,
     USER_PROFILE: "/user/profile/6322cd14b48b514e1f3f16a8"
   },
   POST:{
