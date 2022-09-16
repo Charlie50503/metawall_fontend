970e26ed6b9a11683bd5a5f62ee303ee1ea2e146
charlie50503 <cate50503@gmail.com>
2022-09-16 13:08:09 +0900

    feat: 添加comment設定

--- src/app/components/comment/comment.component.ts
+++ src/app/components/comment/comment.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { comment } from 'src/app/interfaces/comment.interface';
 
 @Component({
   selector: 'app-comment',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./comment.component.scss']
 })
 export class CommentComponent implements OnInit {
-
+  @Input() comment!:comment;
   constructor() { }
 
   ngOnInit(): void {
