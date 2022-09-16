8ced57ce2643dd8353a7db9993b65072b28705d9
charlie50503 <cate50503@gmail.com>
2022-09-16 13:19:28 +0900

    feat: 在sidebar添加使用者大頭貼跟用戶名

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ConfigService } from 'src/app/services/config.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -6,8 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
-
-  constructor() { }
+  userProfile = this.configService.userProfile;
+  userImgUrl = this.configService.userImgUrl;
+  constructor(
+    private configService:ConfigService
+  ) { }
 
   ngOnInit(): void {
   }
