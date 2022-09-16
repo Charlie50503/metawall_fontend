d06069f491788a855ce68295b2f3d0613818afc6
charlie50503 <cate50503@gmail.com>
2022-09-16 14:00:49 +0900

    feat: 刪除不需要的代碼

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -9,28 +9,12 @@ import API_LIST from './api/api-list';
   providedIn: 'root'
 })
 
-
-
-
-
 export class PostService {
 
   constructor(private http: HttpClient) { }
 
   public getAllPost(
   ): Observable<post[]> {
-    // const httpOptions = {
-    //   headers: new HttpHeaders({
-    //     'Content-Type': 'application/json',
-    //     'Access-Control-Allow-Origin': '*',
-    //     'Access-Control-Allow-Credentials': 'true',
-    //     'Access-Control-Allow-Headers': 'Content-Type',
-    //     'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE',
-    //     'key': 'x-api-key',
-    //   })
-    // }
-    // const URL = 'http://localhost:3000/post/all-post';
-    // return this.http.get<any>(URL,httpOptions);
     return this.http.get<httpResponse>(API_LIST.GET.ALL_POST).pipe(
       map(response=>response.data)
     );
