aa542cfb8589b99b853b700bb285c933fcaec26c
charlie50503 <cate50503@gmail.com>
2022-09-16 12:53:48 +0900

    refactor: 將使用者大頭貼設定放置在config.service

--- src/app/services/config.service.ts
+++ src/app/services/config.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { lastValueFrom, map } from 'rxjs';
 import { httpResponse } from '../interfaces/http.interface';
 import { user } from '../interfaces/user.interface';
+import { UserImgUrlService } from './user-img-url.service';
 
 @Injectable({
   providedIn: 'root'
@@ -11,15 +12,21 @@ import { user } from '../interfaces/user.interface';
 export class ConfigService {
   private _config: any;
   private _userProfile!: user;
+  private _userImgUrl!:string;
   get config() { return this._config; }
   get userProfile() { return this._userProfile; }
+  get userImgUrl() { return this._userImgUrl; }
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    private userImgUrlService:UserImgUrlService
+    ) { }
 
   async load(): Promise<void> {
     this._userProfile = await lastValueFrom(this.http.get<httpResponse>('/user/profile/6322cd14b48b514e1f3f16a8')
       .pipe(
         map(response => response.data)
       ))
+    this._userImgUrl = this.userImgUrlService.setUserImgUrl(this._userProfile.avatar,this._userProfile.sex)
   }
 }
