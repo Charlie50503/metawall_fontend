b7b24464a4b478f74ae51a6134a05c310430dce5
charlie50503 <cate50503@gmail.com>
2022-09-16 12:33:45 +0900

    feat: 再header-profile添加用戶大頭貼跟用戶名

--- src/app/components/header-profile/header-profile.component.ts
+++ src/app/components/header-profile/header-profile.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, SimpleChanges } from '@angular/core';
+import { user } from 'src/app/interfaces/user.interface';
+import { ConfigService } from 'src/app/services/config.service';
+import { UserImgUrlService } from 'src/app/services/user-img-url.service';
 
 @Component({
   selector: 'app-header-profile',
@@ -6,10 +9,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header-profile.component.scss']
 })
 export class HeaderProfileComponent implements OnInit {
+  userProfile:user = this.configService.userProfile;
+  userImgUrl = this.userImgUrlService.setUserImgUrl(this.userProfile.avatar,this.userProfile.sex)
 
-  constructor() { }
+  constructor(
+    private configService:ConfigService,
+    private userImgUrlService:UserImgUrlService
+  ) { }
 
   ngOnInit(): void {
-  }
 
+  }
 }
