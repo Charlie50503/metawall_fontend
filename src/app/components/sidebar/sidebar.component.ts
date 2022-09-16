9345142c34927ea8e2d2f10169bd46faae83fcfb
charlie50503 <cate50503@gmail.com>
2022-09-16 16:04:56 +0900

    feat: 添加link設定到sidebar

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { ConfigService } from 'src/app/services/config.service';
 
 @Component({
@@ -10,10 +11,25 @@ export class SidebarComponent implements OnInit {
   userProfile = this.configService.userProfile;
   userImgUrl = this.configService.userImgUrl;
   constructor(
-    private configService:ConfigService
+    private configService:ConfigService,
+    private router:Router
   ) { }
 
   ngOnInit(): void {
+  }
+
+  linkToPostEdit(){
+    this.router.navigate(["/main/post-edit"])
+  }
+  linkToFollowing(){
+    this.router.navigate(["/main/following"])
+  }
+  linkToLikeList(){
+    this.router.navigate(["/main/like-list"])
+  }
+
+  linkToPersonalProfile(){
+    this.router.navigate(["/main/personal-profile"])
   }
 
 }
