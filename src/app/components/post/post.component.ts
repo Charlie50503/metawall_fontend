2982b8280ff8b586825fa1c869a8e48bf812c5f6
charlie50503 <cate50503@gmail.com>
2022-09-16 14:00:35 +0900

    feat: 添加留言功能

--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -1,6 +1,9 @@
+import { CommentService } from './../../services/comment.service';
 import { Component, Input, OnInit } from '@angular/core';
 import { post } from 'src/app/interfaces/post.interface';
 import { ConfigService } from 'src/app/services/config.service';
+import { commentCreateBody } from 'src/app/interfaces/request/comment-create';
+import { comment } from 'src/app/interfaces/comment.interface';
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
@@ -10,14 +13,34 @@ export class PostComponent implements OnInit {
   @Input() post!:post;
 
   userProfile = this.configService.userProfile;
-  userImgUrl = this.configService.userImgUrl
+  userImgUrl = this.configService.userImgUrl;
+
+  commentMessage:string = "";
   ary = [1,2]
   constructor(
     private configService: ConfigService,
+    private commentService:CommentService
   ) { }
 
   ngOnInit(): void {
   }
 
+  sendComment(){
+    const body:commentCreateBody = {
+      comment:this.commentMessage
+    }
+    this.commentService.postCommentCreate(this.post._id,body).subscribe(payload=>{
+      console.log(payload);
+      this.updatePostComment(payload.comment)
+      this.cleanCommentMessage()
+    })
+  }
 
+  updatePostComment(comment:comment | any){
+    this.post.comments.push(comment)
+  }
+
+  cleanCommentMessage(){
+    this.commentMessage = ""
+  }
 }
