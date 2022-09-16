abc32b7acf198c2ed750d783db237e0402347859
charlie50503 <cate50503@gmail.com>
2022-09-16 13:59:34 +0900

    feat: 添加comment-create api 到 api-list

--- src/app/services/api/api-list.ts
+++ src/app/services/api/api-list.ts
@@ -2,6 +2,9 @@ const API_LIST = {
   GET: {
     ALL_POST: "/post/all-post",
     USER_PROFILE: "/user/profile/6322cd14b48b514e1f3f16a8"
+  },
+  POST:{
+    COMMENT_CREATE: (postId:string) => `/comment/create/${postId}`
   }
 }
 
