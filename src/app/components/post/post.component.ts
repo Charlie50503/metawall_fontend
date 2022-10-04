569f503992db25544b4daea70ded070527792771
charlie50503 <cate50503@gmail.com>
2022-10-04 17:46:40 +0900

    feat: 添加按讚取消讚功能

--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -1,46 +1,86 @@
+import { WarningToastService } from './../../services/warning-toast.service';
+import { LikeService } from './../../services/like.service';
 import { CommentService } from './../../services/comment.service';
 import { Component, Input, OnInit } from '@angular/core';
 import { post } from 'src/app/interfaces/post.interface';
 import { ConfigService } from 'src/app/services/config.service';
 import { commentCreateBody } from 'src/app/interfaces/request/comment-create';
 import { comment } from 'src/app/interfaces/comment.interface';
+
+
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.scss']
 })
 export class PostComponent implements OnInit {
-  @Input() post!:post;
+  @Input() post!: post;
 
   userProfile = this.configService.userProfile;
   userImgUrl = this.configService.userImgUrl;
 
-  commentMessage:string = "";
-  ary = [1,2]
+  commentMessage: string = "";
   constructor(
     private configService: ConfigService,
-    private commentService:CommentService
+    private commentService: CommentService,
+    private likeService: LikeService,
+    private warningToastService: WarningToastService
   ) { }
 
   ngOnInit(): void {
   }
 
-  sendComment(){
-    const body:commentCreateBody = {
-      comment:this.commentMessage
+  sendComment() {
+    const body: commentCreateBody = {
+      comment: this.commentMessage
     }
-    this.commentService.postCommentCreate(this.post._id,body).subscribe(payload=>{
+    this.commentService.postCommentCreate(this.post._id, body).subscribe(payload => {
       console.log(payload);
       this.updatePostComment(payload.comment)
       this.cleanCommentMessage()
     })
   }
 
-  updatePostComment(comment:comment | any){
+  updatePostComment(comment: comment | any) {
     this.post.comments.push(comment)
   }
 
-  cleanCommentMessage(){
+  cleanCommentMessage() {
     this.commentMessage = ""
+  }
+
+  toggleLike() {
+    console.log(this.post.likes.some(userProfile=>{
+        console.log(userProfile._id,this.userProfile._id);
+
+      return userProfile._id===this.userProfile._id
+      }
+    ));
+
+    if(!this.post.likes.some(userProfile=>userProfile._id===this.userProfile._id)){
+      this.likeService.addLike(this.post._id).subscribe(
+        {
+          next: data => {
+
+            this.post = data.post
+          },
+          error: error => {
+            this.warningToastService.setWarningToastMessage(error.message)
+          }
+        }
+      )
+    }else {
+      this.likeService.deleteLike(this.post._id).subscribe(
+        {
+          next: data => {
+            this.post = data.post
+          },
+          error: error => {
+            this.warningToastService.setWarningToastMessage(error.message)
+          }
+        }
+      )
+    }
+
   }
 }
---
+++ src/app/services/like.service.ts
@@ -0,0 +1,48 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { httpResponse } from '../interfaces/http.interface';
+import { post } from '../interfaces/post.interface';
+import API_LIST from './api/api-list';
+
+
+interface likeResponse {
+  message:string,
+  post:post
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class LikeService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+
+  public addLike(postId: string
+  ): Observable<likeResponse> {
+    const body = ""
+    return this.http.post<httpResponse>(API_LIST.POST.ADD_LIKE(postId),body).pipe(
+      map(response => {
+        if(response.status!=="success") {
+          throw new Error(response.message);
+        }
+        return response.data
+      })
+    );
+  }
+  public deleteLike(postId: string
+  ): Observable<likeResponse> {
+    return this.http.delete<httpResponse>(API_LIST.DELETE.DELETE_LIKE(postId)).pipe(
+      map(response => {
+        if(response.status!=="success") {
+          throw new Error(response.message);
+        }
+        return response.data
+      })
+    );
+  }
+}
