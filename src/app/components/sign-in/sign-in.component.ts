525b0e8d4f3d8964eabc9c9fad111c9ad5711f25
charlie50503 <cate50503@gmail.com>
2022-10-05 13:55:30 +0900

    refactor: sign-in功能component化

--- src/app/components/sign-in/sign-in.component.ts
+++ src/app/components/sign-in/sign-in.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { signInBody } from 'src/app/interfaces/request/sign-in';
+import { ConfigService } from 'src/app/services/config.service';
+import { LoginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'app-sign-in',
@@ -6,10 +11,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-in.component.scss']
 })
 export class SignInComponent implements OnInit {
-
-  constructor() { }
+  signInForm: FormGroup = new FormGroup({
+    email: new FormControl("", [Validators.required, Validators.email]),
+    password: new FormControl("", [Validators.required,Validators.minLength(8)]),
+  });
+  errorMessage:string = "";
+  constructor(
+    private loginService:LoginService,
+    private configService:ConfigService,
+    private router : Router
+  ) { }
 
   ngOnInit(): void {
   }
 
+
+  login() {
+    const parmas:signInBody = {
+      email:this.signInForm.value["email"],
+      password:this.signInForm.value["password"]
+    }
+    this.loginService.signIn(parmas).subscribe({
+      next:(data)=>{
+        localStorage.setItem("token",data.token)
+        this.configService.setId(data._id)
+        this.router.navigate(["/main/all-post"])
+      },
+      error:(error)=>{
+        this.errorMessage = error.message
+      }
+    })
+  }
 }
+
