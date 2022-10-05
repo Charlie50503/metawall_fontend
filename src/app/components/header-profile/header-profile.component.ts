9d8ccfe50a0c7e7fa60ca653b55526cdb02fbecb
charlie50503 <cate50503@gmail.com>
2022-10-05 15:51:23 +0900

    feat: 添加導航bar

--- src/app/components/header-profile/header-profile.component.ts
+++ src/app/components/header-profile/header-profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, SimpleChanges } from '@angular/core';
+import { Router } from '@angular/router';
 import { user } from 'src/app/interfaces/user.interface';
 import { ConfigService } from 'src/app/services/config.service';
 
@@ -8,14 +9,34 @@ import { ConfigService } from 'src/app/services/config.service';
   styleUrls: ['./header-profile.component.scss']
 })
 export class HeaderProfileComponent implements OnInit {
-  userProfile:user = this.configService.userProfile;
-  userImgUrl:string = this.configService.userImgUrl;
+  userProfile: user = this.configService.userProfile;
+  userImgUrl: string = this.configService.userImgUrl;
 
+  isShowNavigatorDropDown = false;
   constructor(
-    private configService:ConfigService,
+    private configService: ConfigService,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
 
+  }
+
+  goUserProfile() {
+    this.router.navigate(["/main/personal-profile"])
+    this.isShowNavigatorDropDown = false
+  }
+  goPersonPost() {
+    // this.router.navigate(["/main/personal-profile"])
+    this.isShowNavigatorDropDown = false
+  }
+  logout() {
+    localStorage.removeItem("metawall-token")
+    this.router.navigate(["/login"])
+    this.isShowNavigatorDropDown = false
+  }
+
+  toggleNavigatorDropDown() {
+    this.isShowNavigatorDropDown = !this.isShowNavigatorDropDown
   }
 }
