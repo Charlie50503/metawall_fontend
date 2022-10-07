f335d130f08025ee4e01a0ae45ade2e57e4eb534
charlie50503 <cate50503@gmail.com>
2022-10-07 15:14:43 +0900

    feat: 在post-personal-profile添加取得個人資料跟追蹤人數功能

--- src/app/components/post-personal-profile/post-personal-profile.component.ts
+++ src/app/components/post-personal-profile/post-personal-profile.component.ts
@@ -1,4 +1,10 @@
+import { UserImgUrlService } from 'src/app/services/user-img-url.service';
+import { ActivatedRoute, ActivatedRouteSnapshot } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
+import { user } from 'src/app/interfaces/user.interface';
+import { FollowService } from 'src/app/services/follow.service';
+import { following } from 'src/app/interfaces/follow.interface';
 
 @Component({
   selector: 'app-post-personal-profile',
@@ -6,10 +12,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-personal-profile.component.scss']
 })
 export class PostPersonalProfileComponent implements OnInit {
-
-  constructor() { }
+  user!: user;
+  imgUrl = "";
+  nickName: string = "";
+  following: following = [];
+  constructor(
+    private userService: UserService,
+    private followService: FollowService,
+    private userImgUrlService: UserImgUrlService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    const userId = this.route.snapshot.paramMap.get("userId") || "";
+    this.userService.userChanged.subscribe(newUser => {
+      this.user = newUser
+      this.imgUrl = this.userImgUrlService.setUserImgUrl(this.user.avatar, this.user.sex)
+    })
+    this.followService.followingChanged.subscribe(newFollowing => {
+      this.following = newFollowing
+    })
+    this.userService.getUserProfile(userId).subscribe(user => {
+      this.user = user
+      this.imgUrl = this.userImgUrlService.setUserImgUrl(this.user.avatar, this.user.sex)
+    })
+    this.followService.getFollowing(userId).subscribe(data => {
+      if (data.following) {
+        this.following = data.following
+      }
+    })
   }
 
 }
