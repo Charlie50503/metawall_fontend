e20f24a70bfa626b17d4db42f657a159feaafd35
charlie50503 <cate50503@gmail.com>
2022-10-07 13:49:25 +0900

    feat: 添加page not found 頁面

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
-import { HeaderComponent } from './components/header/header.component';
 import { LoginComponent } from './pages/login/login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { MainComponent } from './pages/main/main.component';
 
 const routes: Routes = [
   { path: "login", component: LoginComponent },
-  { path: "header", component: HeaderComponent },
+  { path: "page-not-found", component: PageNotFoundComponent },
+  {
+    path: "**",
+    redirectTo: "/page-not-found"
+  },
 ];
 
 @NgModule({
