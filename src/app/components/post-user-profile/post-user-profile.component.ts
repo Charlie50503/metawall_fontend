0b56dce5e741fd413e3f5898c4ad39ad9f7741ce
charlie50503 <cate50503@gmail.com>
2022-10-07 12:05:29 +0900

    feat: 針對 post區塊的頭像及使用者名稱添加導向到person-post頁面功能

--- src/app/components/post-user-profile/post-user-profile.component.ts
+++ src/app/components/post-user-profile/post-user-profile.component.ts
@@ -1,7 +1,9 @@
+import { PostListService } from 'src/app/services/post-list.service';
 import { DatetimeFormatService } from './../../services/datetime-format.service';
 import { UserImgUrlService } from '../../services/user-img-url.service';
 import { Component, Input, OnInit, SimpleChanges } from '@angular/core';
 import { post } from 'src/app/interfaces/post.interface';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-post-user-profile',
   templateUrl: './post-user-profile.component.html',
@@ -13,19 +15,28 @@ export class PostUserProfileComponent implements OnInit {
     this.createdDatetime = this.datetimeFormatService.datetimeFormat(createdAt,"yyyy/MM/dd H:m") || ""
   }
   @Input() set creator(creator:post["creator"]){
+    this.creatorId = creator._id
     this.imgUrl = this.userImgUrlService.setUserImgUrl(creator.avatar,creator.sex)
     this.nickName = creator.nickName
   }
 
+  public creatorId!:string;
   public imgUrl!:string;
   public createdDatetime!:string;
   public nickName!:string;
 
   constructor(
     private userImgUrlService:UserImgUrlService,
-    private datetimeFormatService:DatetimeFormatService
+    private datetimeFormatService:DatetimeFormatService,
+    private postListService:PostListService,
+    private router : Router
   ) { }
 
   ngOnInit(): void {
+  }
+
+  public goPersonPostPage(){
+    this.postListService.setTargetUserId(this.creatorId);
+    this.router.navigate(["/main/person-post"])
   }
 }
