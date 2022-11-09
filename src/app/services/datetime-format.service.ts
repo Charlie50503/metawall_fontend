525973a18d6b07510e955691f8256440a3146373
charlie50503 <cate50503@gmail.com>
2022-11-09 17:13:41 +0800

    feat: 添加日期相減共通方法

--- src/app/services/datetime-format.service.ts
+++ src/app/services/datetime-format.service.ts
@@ -6,10 +6,15 @@ import { Injectable } from '@angular/core';
 })
 export class DatetimeFormatService {
   constructor(
-    private datePipe:DatePipe
+    private datePipe: DatePipe
   ) { }
 
   datetimeFormat(datetimeStr: string, format: string) {
     return this.datePipe.transform(datetimeStr, format);
   }
+
+  getDateDifference(Date1: Date, Date2: Date) { // date object 日期格式
+    let milliseconds_Time = Date2.getTime() - Date1.getTime();
+    return milliseconds_Time / (1000 * 3600 * 24);
+  };
 }
