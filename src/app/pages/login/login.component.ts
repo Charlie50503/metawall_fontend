c9e93f69f0e6d0cac96ea5405815511a21f196ce
charlie50503 <cate50503@gmail.com>
2022-11-09 17:24:46 +0800

    feat: 添加更新密碼以及在登入頁面沒有token直接跳轉到需要token頁面時的錯誤處理
    補出：此功能善未完善仍需要調整

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,8 @@
+import { ActivatedRoute, NavigationStart, Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { ToastService } from 'src/app/services/toast.service';
+import { filter } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -8,21 +12,53 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
   isSignInPage = true;
   isLoading = false;
+
+  subscription = new Subscription;
   constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private toastService: ToastService
   ) { }
 
   ngOnInit(): void {
+    this.subscription = this.router.events.pipe(filter(event => event instanceof NavigationStart))
+      .subscribe((event) => {
+        console.log("2222");
+        const toastMessage = this.route.snapshot.queryParamMap.get("toastMessage")
+        const status = this.route.snapshot.queryParamMap.get("status")
+        console.log(toastMessage, status);
+
+        if (toastMessage && status) {
+          this.showToastMessage(status, toastMessage)
+        }
+        // You only receive NavigationStart events
+      });
   }
 
-  isSignUp(){
+  ngOnDestroy(): void {
+    //Called once, before the instance is destroyed.
+    //Add 'implements OnDestroy' to the class.
+    this.subscription.unsubscribe()
+  }
+
+  showToastMessage(status: string, message: string) {
+    setTimeout(() => { // TODO 這個時機點需要優化，應該有更好的解法
+      if (status === "success") {
+        this.toastService.setSuccessToastMessage(message);
+      } else {
+        this.toastService.setWarningToastMessage(message);
+      }
+    }, 0)
+  }
+  isSignUp() {
     this.isSignInPage = false;
   }
 
-  isSignIn(){
+  isSignIn() {
     this.isSignInPage = true;
   }
 
-  onLoading(value : boolean){
+  onLoading(value: boolean) {
     this.isLoading = value;
   }
 }
--- src/app/router-guard/auth-guard.service.ts
+++ src/app/router-guard/auth-guard.service.ts
@@ -6,7 +6,7 @@ import {
   Router,
   UrlTree,
 } from '@angular/router';
-import { map, Observable } from 'rxjs';
+import { catchError, map, Observable, of } from 'rxjs';
 
 import { AuthService } from '../services/auth/auth.service';
 
@@ -14,17 +14,20 @@ import { AuthService } from '../services/auth/auth.service';
   providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
-  constructor(private router: Router, private authService: AuthService) {}
+  constructor(private router: Router, private authService: AuthService) { }
 
   canActivate(): Observable<boolean | UrlTree> {
     return this.authService.isAuthenticated().pipe(
+      catchError((err, caught) => {
+        return of(this.router.navigate(["/login"], { queryParams: { status:"error", toastMessage: "驗證失敗" } }))
+      }),
       map((authenticated) => {
         if (authenticated) {
           return true;
         } else {
           return this.router.parseUrl('/login');
         }
-      })
+      }),
     );
   }
 
